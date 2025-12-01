117c225242a6b4f7a178ae194fa5f38c14d7db8c
NavecTaty <navecsarontaty@gmail.com>
2025-12-01 15:44:12 +0100

    Initialisation de la base de données.

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description de la configuration du projet
+ */
+//Paramètres de configuration Mysql
+define('DB_HOST','localhost');//adresse du serveur,localhost pour le site en local
+define('DB_NAME', 'recettesbd'); //notre base de données
+define('DB_USER','root');// root utilisateur par défaut de Mysql
+define('DB_PASS','');//pas de mot de passe
+define('DB_CHARSET', 'utf8mb4');//encodage
