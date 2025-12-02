f29d88689af861e398fe4d592ad573f0fec1663d
Giovana Tchuente <prisca082005@gmail.com>
2025-12-02 13:35:39 +0100

    ajout du fichier header

---
+++ src/include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Cocktail Guide</title>
+   <link rel="stylesheet" href="../Ressources/css/style.css">
+    <link rel="stylesheet" href="../Ressources/css/header.css">
+    <link rel="stylesheet" href="../Ressources/css/footer.css">
+     <link rel="stylesheet" href="../Ressources/css/texte.css">
+    <link rel="stylesheet" href="../Ressources/css/profil1.css">
+
+
+
+</head>
+<body>
+<header class= "header">
+    <h1>Recettes</h1>
+    <nav>
+        <a href= "acceuil.php">Acceuil</a>
+        <a href= "recettes.php">Recettes</a>
+        <a href= "favoris.php">Favoris</a>
+        <a href= "profil.php">profil</a>
+    </nav>
+    <hr>
+</header>
+
