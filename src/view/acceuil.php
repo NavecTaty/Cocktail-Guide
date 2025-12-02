e37db7c9729eddc5dfd5a7b03e47a1d1d88b8312
Giovana Tchuente <prisca082005@gmail.com>
2025-12-02 13:37:26 +0100

    ajout de la page d'acceuil

---
+++ src/view/acceuil.php
@@ -0,0 +1,19 @@
+
+     <?php include('../include/header.php'); ?>
+    <div class= "recherche-container">
+        <form action="recherche.php" method="GET">
+            <input type="text" name="q" placeholder="Rechercher: cocktails , ingrédiants..." required>
+            <button type="submit">Rechercher</button>
+        </form>
+    </div>
+    <div class = "texte">
+        <p>Qui nous sommes ??</p>
+        <p>Bienvenue sur notre Cocktail Guide ! Nous sommes passionnés par l’art des cocktails et la découverte de nouvelles saveurs. Nous aimons partager des recettes créatives et accessibles, que ce soit pour les débutants ou les amateurs expérimentés.
+            Pour nous, préparer un cocktail n’est pas seulement suivre une recette : c’est une occasion de laisser libre cours à sa créativité, de s’amuser et de partager des moments conviviaux avec ses proches. Sur notre site, vous trouverez des guides, 
+            des astuces et nos recettes préférées, toutes pensées pour rendre votre expérience de mixologie agréable et mémorable.</p>
+        <p>Découvrez quelques de nos recettes et boissons</p>
+</div>
+
+    <?php include('../include/footer.php'); ?>
+</body>
+</html>
