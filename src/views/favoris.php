9dc4ac90283d7c93e5237ba25b87921060289595
Navec taty <tatynavec95@gmail.com>
2025-12-30 10:41:18 +0100

    Un utilisateur non connecté peut aussi voir ses favoris sans etre connecté.

--- src/views/favoris.php
+++ src/views/favoris.php
@@ -4,11 +4,53 @@
  */
 require_once __DIR__ . '/../models/recettesFavorites.php';
 require_once __DIR__ . '/../models/recette.php';
+
+//utilisateur non connécté
 if (!isset($_SESSION['user'])) {?>
-    <p class="aucune-recette">Connectes-toi, tes favoris t'attendent vite !</p>";
-   <?php return;
+
+    <h2 class="favoriTitre">Mes recettes favorites</h2>
+    <?php if (!empty($_SESSION['favoris_temp'])) :?>
+            <div class="recettes-grille">
+                    <?php foreach ( $_SESSION['favoris_temp'] as $idRecette): ?>
+                    <?php
+                        $r = getRecetteById($idRecette);
+                        $image = getRecettePhoto($r['titre']);
+                        $apercu = mb_substr($r['preparation'], 0, 120);
+                        $estFavori = true;
+                    ?>
+
+                    <div class="recette-card">
+                        <!---- icone pour retirer comme favori  ---->
+                        <form method="post" action="index.php?page=favoris_action" class="favori-form">
+                            <input type="hidden" name="id_recette" value="<?= $r['id_recette'] ?>">
+                            <input type="hidden" name="action" value="supprimer">
+                            <button type="submit" class="favori-btn actif">
+                            ★
+                            </button>
+                        </form>
+                        <a href="index.php?page=recette&id=<?= $r['id_recette'] ?>" class="recette-link">
+                                <img src="<?= $image ?>"
+                                        alt="Photo <?= htmlspecialchars($r['titre']) ?>"
+                                onerror="this.onerror=null;this.src='/Cocktail-Guide/src/Ressources/Photos/defaut.jpg';">
+
+                                <h4><?= htmlspecialchars($r['titre']) ?></h4>
+                        </a>
+                        <p><?= htmlspecialchars($apercu) ?>...</p>
+            
+                    </div>
+                     <?php endforeach; ?>
+                </div>
+
+
+        <?php else:?>
+                <p class="aucune-recette">Aucune recette favorite.</p>
+      <?php 
+        endif;
+    
+    return;
 }
 
+//Utilisateur connecté
 $favoris = getFavorisUtilisateur($_SESSION['user']['id']);
 ?>
 
@@ -50,6 +92,3 @@ $favoris = getFavorisUtilisateur($_SESSION['user']['id']);
 
 
         <?php endif; ?>
-    </main>
-
-</div>
